76a27340871c8803bc9a594f868a86c01ef79c04
0nly1 <42980257+0nly1@users.noreply.github.com>
2018-10-15 13:01:10 +0300

    Update UserController.cs

--- TelegrammAspMvcDotNetCoreBot/Controllers/UserController.cs
+++ TelegrammAspMvcDotNetCoreBot/Controllers/UserController.cs
@@ -1,259 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System;
+using System.Xml.Linq;
 using System.Xml;
 
-namespace TelegrammAspMvcDotNetCoreBot.Controllers
+namespace XmlUsers
 {
-    public class UserController
-	{
-		public static bool CheckUserAv(long UserId)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
+    class User
+    {
+        public static void CreateUser(long UserId)
+        {
+            XDocument xDoc = XDocument.Load("users.xml");
+            XElement xRoot = xDoc.Element("users");
+            XElement newUser = new XElement("user",
+                new XAttribute("id", UserId),
+                new XElement("university", ""), 
+                new XElement("faculty", ""),
+                new XElement("course", ""),
+                new XElement("group", ""));
+            xDoc.Save("users.xml");
+        }
 
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
+        public static bool CheckUser(long UserId) //Проверка существования пользователя
+        {
+            //Делаю через System.Xml
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNode node = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
+            if (node != null)
+            {
+                return true;
+            }
+            return false;
+        }
 
-			if (childnode != null)
-				return true;
+        public static void RecreateUser(long UserId)
+        {
 
-			return false;
-		}
+            //Делаю через System.Xml
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNode node = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
 
-		public static void CheckUser(long UserId)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
+            xRoot.RemoveChild(node);
+            xDoc.Save("users.xml");
 
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
+            CreateUser(UserId);
+        }
 
-			if (childnode == null)
-				CreateUser(UserId);
-			else
-				ReCreateUser(UserId);
-		}
+        public static void EditUser(long UserId, string type, string param)
+        {
+            //Делаю через System.Xml
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNode node = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
 
-		public static void ReCreateUser(long UserId)
-		{
+            foreach (XmlNode find in node.ChildNodes)
+            {
+                if (find.Name == type)
+                {
+                    node.RemoveChild(find);
+                    XmlElement el = xDoc.CreateElement(type);
+                    XmlText text = xDoc.CreateTextNode(param);
+                    node.AppendChild(el);
+                    el.AppendChild(text);
+                    xDoc.Save("users.xml");
+                    break;
+                }
+            }
+        }
+        public static string CheckUserElements(long UserId, string type) //ЧТО МЫ ВЫВОДИМ? Должны ли мы искать по определенному type? 
+        {
+            //Делаю через System.Xml
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            XmlNode node = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
 
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			xRoot.RemoveChild(childnode);
-			xDoc.Save("users.xml");
-
-			CreateUser(UserId);
-		}
-
-		public static void CreateUser(long UserId)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			// создаем новый элемент user 
-			XmlElement userElem = xDoc.CreateElement("user");
-			// создаем атрибут name 
-			XmlAttribute idAttr = xDoc.CreateAttribute("id");
-			// создаем элементы university и faculty 
-			XmlElement universityElem = xDoc.CreateElement("university");
-			XmlElement facultyElem = xDoc.CreateElement("faculty");
-			XmlElement courseElem = xDoc.CreateElement("course");
-			XmlElement groupElem = xDoc.CreateElement("group");
-			// создаем текстовые значения для элементов и атрибута 
-			XmlText idText = xDoc.CreateTextNode(UserId.ToString());
-			XmlText universityText = xDoc.CreateTextNode("");
-			XmlText facultyText = xDoc.CreateTextNode("");
-			XmlText courseText = xDoc.CreateTextNode("");
-			XmlText groupText = xDoc.CreateTextNode("");
-
-			//добавляем узлы 
-			idAttr.AppendChild(idText);
-			universityElem.AppendChild(universityText);
-			facultyElem.AppendChild(facultyText);
-			courseElem.AppendChild(courseText);
-			groupElem.AppendChild(groupText);
-
-			userElem.Attributes.Append(idAttr);
-			userElem.AppendChild(universityElem);
-			userElem.AppendChild(facultyElem);
-			userElem.AppendChild(courseElem);
-			userElem.AppendChild(groupElem);
-
-			xRoot.AppendChild(userElem);
-			xDoc.Save("users.xml");
-		}
-
-		public static void EditUser(long UserId, string param)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.InnerText == "")
-				{
-					EditUserInf(UserId, param, child.Name);
-					break;
-
-					if (child.Name == "university")
-					{
-						EditUserUniversity(UserId, param);
-						break;
-					}
-					if (child.Name == "faculty")
-					{
-						EditUserFaculty(UserId, param);
-						break;
-					}
-					if (child.Name == "course")
-					{
-						EditUserCourse(UserId, param);
-						break;
-					}
-					if (child.Name == "group")
-					{
-						EditUserGroup(UserId, param);
-						break;
-					}
-				}
-			}
-		}
-
-		private static void EditUserInf(long UserId, string param, string type)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == type)
-				{
-					childnode.RemoveChild(child);
-					XmlElement typeElem = xDoc.CreateElement("type");
-					XmlText typeText = xDoc.CreateTextNode(param);
-					typeElem.AppendChild(typeText);
-					childnode.AppendChild(typeElem);
-					xDoc.Save("users.xml");
-					break;
-				}
-			}
-		}
-
-		public static void EditUserUniversity(long UserId, string param)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == "university")
-				{
-					childnode.RemoveChild(child);
-					XmlElement universityElem = xDoc.CreateElement("university");
-					XmlText universityText = xDoc.CreateTextNode(param);
-					universityElem.AppendChild(universityText);
-					childnode.AppendChild(universityElem);
-					xDoc.Save("users.xml");
-					break;
-				}
-			}
-		}
-
-		public static void EditUserFaculty(long UserId, string param)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == "faculty")
-				{
-					childnode.RemoveChild(child);
-					XmlElement facultyElem = xDoc.CreateElement("faculty");
-					XmlText facultyText = xDoc.CreateTextNode(param);
-					facultyElem.AppendChild(facultyText);
-					childnode.AppendChild(facultyElem);
-					xDoc.Save("users.xml");
-					break;
-				}
-			}
-		}
-
-		public static void EditUserCourse(long UserId, string param)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == "course")
-				{
-					childnode.RemoveChild(child);
-					XmlElement courseElem = xDoc.CreateElement("course");
-					XmlText courseText = xDoc.CreateTextNode(param);
-					courseElem.AppendChild(courseText);
-					childnode.AppendChild(courseElem);
-					xDoc.Save("users.xml");
-					break;
-				}
-			}
-		}
-
-		public static void EditUserGroup(long UserId, string param)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == "group")
-				{
-					childnode.RemoveChild(child);
-					XmlElement groupElem = xDoc.CreateElement("group");
-					XmlText groupText = xDoc.CreateTextNode(param);
-					groupElem.AppendChild(groupText);
-					childnode.AppendChild(groupElem);
-					xDoc.Save("users.xml");
-					break;
-				}
-			}
-		}
-
-		public static string Get(long UserId, string Type)
-		{
-			XmlDocument xDoc = new XmlDocument();
-			xDoc.Load("users.xml");
-			XmlElement xRoot = xDoc.DocumentElement;
-			XmlNode childnode = xRoot.SelectSingleNode("user[@id='" + UserId + "']");
-
-
-			foreach (XmlNode child in childnode.ChildNodes)
-			{
-				if (child.Name == Type)
-				{
-					return child.InnerText;
-				}
-			}
-
-			return "nothing";
-
-		}
-	}
+            string ans ="";
+            foreach (XmlNode find in node.ChildNodes) //Запутался
+            {
+                if (find.Name == type)
+                {
+                    if (find.InnerText != "")
+                    {
+                        ans = "";
+                    }
+                    else
+                    {
+                        ans = find.InnerText;
+                    }
+                    return ans;
+                }
+            }
+            return ans;
+        }
+    }
 }
